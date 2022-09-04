9dbfc198dd6c77429d1b4a3346ad64937127f613
Gabriel Tomsic <gabriel@macmax.local>
2022-09-03 19:40:11 -0600

    Changing theme font size and color complete

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 // SIDEBAR
 const menuItems = document.querySelectorAll('.menu-item')
+// MESSAGES
+const messagesNotification = document.querySelector('#messages-notifications')
+const messages = document.querySelector('.messages')
+const message = messages.querySelectorAll('.message')
+const messageSeach = document.querySelector('#message-search')
+// THEME
+const theme = document.querySelector('#theme')
+const themeModal = document.querySelector('.customize-theme')
+const fontSizes = document.querySelectorAll('.choose-size span')
+var root = document.querySelector(':root')
+const colorPalette = document.querySelectorAll('.choose-color span')
 
 // Remove all active class from menu item before adding
 const changeActiveItem = () => {
@@ -12,10 +23,110 @@ menuItems.forEach((item) => {
     item.classList.add('active')
     if (item.id != 'notifications') {
       document.querySelector('.notification-popup').style.display = 'none'
-      document.querySelector('.notification-count').style.display = 'block'
     } else {
       document.querySelector('.notification-popup').style.display = 'block'
       document.querySelector('.notification-count').style.display = 'none'
     }
+  })
+})
+
+// ======================= MESSAGES ===================
+// Search for chat messages
+const searchMessage = () => {
+  const val = messageSeach.value.toLowerCase()
+  message.forEach((chat) => {
+    let name = chat.querySelector('h5').textContent.toLocaleLowerCase()
+    if (name.indexOf(val) != -1) {
+      chat.style.display = 'flex'
+    } else {
+      chat.style.display = 'none'
+    }
+  })
+}
+messageSeach.addEventListener('keyup', searchMessage)
+
+// Highlight messages card when clicking the menu item
+messagesNotification.addEventListener('click', () => {
+  messages.style.boxShadow = '0 0 1rem var(--color-primary)'
+  messagesNotification.querySelector('.notification-count').style.display =
+    'none'
+  setTimeout(() => {
+    messages.style.boxShadow = 'none'
+  }, 2000)
+})
+
+// THEME/DISPLAY CUSTOMIZATION
+// Open modal
+const openThemeModal = () => {
+  themeModal.style.display = 'grid'
+}
+// Close modal
+const closeThemeModal = (e) => {
+  if (e.target.classList.contains('customize-theme')) {
+    themeModal.style.display = 'none'
+    changeActiveItem()
+  }
+}
+themeModal.addEventListener('click', closeThemeModal)
+theme.addEventListener('click', openThemeModal)
+
+// FONTS ============================
+const removeSizeSelector = () => {
+  fontSizes.forEach((size) => {
+    size.classList.remove('active')
+  })
+}
+fontSizes.forEach((size) => {
+  size.addEventListener('click', () => {
+    removeSizeSelector()
+    let fontSize
+    size.classList.toggle('active')
+    if (size.classList.contains('font-size-1')) {
+      fontSize = '10px'
+      root.style.setProperty('--sticky-top-left', '5.4rem')
+      root.style.setProperty('--sticky-top-right', '5.4rem')
+    } else if (size.classList.contains('font-size-2')) {
+      fontSize = '13px'
+      root.style.setProperty('--sticky-top-left', '5.4rem')
+      root.style.setProperty('--sticky-top-right', '-7rem')
+    } else if (size.classList.contains('font-size-3')) {
+      fontSize = '16px'
+      root.style.setProperty('--sticky-top-left', '-2rem')
+      root.style.setProperty('--sticky-top-right', '-17rem')
+    } else if (size.classList.contains('font-size-4')) {
+      fontSize = '19px'
+      root.style.setProperty('--sticky-top-left', '-5rem')
+      root.style.setProperty('--sticky-top-right', '-25rem')
+    } else if (size.classList.contains('font-size-5')) {
+      fontSize = '22px'
+      root.style.setProperty('--sticky-top-left', '-12rem')
+      root.style.setProperty('--sticky-top-right', '-35rem')
+    }
+    // Change the main html font-size
+    document.querySelector('html').style.fontSize = fontSize
+  })
+})
+
+// Changing color pallete
+const removeColorActive = () => {
+  colorPalette.forEach((color) => color.classList.remove('active'))
+}
+colorPalette.forEach((color) => {
+  color.addEventListener('click', () => {
+    removeColorActive()
+    let primaryHue
+    if (color.classList.contains('color-1')) {
+      primaryHue = 252
+    } else if (color.classList.contains('color-2')) {
+      primaryHue = 52
+    } else if (color.classList.contains('color-3')) {
+      primaryHue = 352
+    } else if (color.classList.contains('color-4')) {
+      primaryHue = 152
+    } else if (color.classList.contains('color-5')) {
+      primaryHue = 202
+    }
+    color.classList.add('active')
+    root.style.setProperty('--primary-color-hue', primaryHue)
   })
 })
